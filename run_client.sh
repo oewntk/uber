8fbd79009b70c5a58b7d9407476a8561db0f8326
oewntk <oewntk@gmail.com>
2026-07-23 08:49:16 +0200

    Server+Client

---
+++ run_client.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Copyright (c) 2021-2026. Bernard Bou.
+
+set -Eeo pipefail
+on_err() {
+  local exit_code=$?
+  local line_no=${BASH_LINENO[0]}
+  echo "Error on line $line_no (exit code: $exit_code)."
+}
+trap on_err ERR
+
+jar=oewn-client-3.0.1-uber.jar
+if [ ! -e "${jar}" ]; then
+  if [ ! -e "target/${jar}" ]; then
+    echo "Non existing uber jar" >&2
+    exit 1
+    fi
+  ln -s "target/${jar}"
+  fi
+if [ ! -e "${jar}" ]; then
+  echo "Non existing uber jar" >&2
+  exit 2
+  fi
+
+java -ea -cp "${jar}" org.oewntk.json.client.MainKt
